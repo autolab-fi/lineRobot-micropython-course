a73e11218fea2ba04d053de493190e3a82423ed5
spivakdm <spivak.dm@gmail.com>
2026-02-12 12:11:52 +0200

    Update module_9.py

--- verifications/module_9.py
+++ verifications/module_9.py
@@ -8,7 +8,7 @@ import numpy as np
 target_points = {
     "fog_of_war_survey": [(30,30), (30, 0)],
     "miniral_scanner_sweep" : [(30,50), (30, 0)],
-    "docking": [(50,61), (0, 0)]
+    "docking": [(50,61), (30, 0)]
 }
 
 # Library call restrictions to discourage bypassing the assignment
